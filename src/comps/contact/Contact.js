2484e83c40cff924efb9bbc8b9c754575cab046b
Lalit1999 <9lalitkumar9@gmail.com>
2019-06-26 12:59:40 +0530

    added about and contact folders

---
+++ src/comps/contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css' ;
+
+class Contact extends React.Component
+{
+	
+	render()
+	{
+		return(
+			<div className = 'color'>
+				This is Contact.	      		
+			</div>
+		) ;
+	}
+}
+
+export default Contact;
