d625fe7b4ff4e37dc32db73c6c270a63ab1fe466
manan999 <aggarwalmanan4@gmail.com>
2019-06-26 12:30:17 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom' ;
 
+// import Home from '' ;
+// import About from '' ;
+// import Contact from '' ;
+// import Products from '' ;
+// import NotFound from './comps/Home/NotFound.js' ;
+// import Header from '' ;
+// import TopBar from '' ;
+ 
 class App extends React.Component
 {
 	
@@ -7,10 +16,28 @@ class App extends React.Component
 	{
 		return(
 			<div>
-				This is App.	      		
+				<BrowserRouter>
+		          <div>
+		          	<TopBar />
+		            <Header />
+		            <Switch>
+		            	Here
+		            </Switch>
+		          </div>
+		        </BrowserRouter>	      		
 			</div>
 		) ;
 	}
 }
 
 export default App;
+
+		              // <Route path='/story/:name' component={BigDetail}/>
+
+
+
+		              // <Route path='/' exact component={Home} />
+		              // <Route path='/about' exact component={About}/>
+		              // <Route path='/contact' exact component={Contact}/>
+		              // <Route path='/products' exact component={Products}/>
+		              // <Route exact component={NotFound} />
