d4e72eb2ff2785aa9715f0b5691d17a5d75e66a1
Lalit1999 <9lalitkumar9@gmail.com>
2019-06-27 22:48:38 +0530

    changes in about file

--- src/comps/about/About.js
+++ src/comps/about/About.js
@@ -8,10 +8,57 @@ class About extends React.Component
 	{
 		return(
 			<div className = 'color'>
-				This is About.	      		
+				<div>
+					<h1>About - Us</h1>
+					<div className = 'layout'>
+						<div>Home</div>
+						<div>About-Us</div> 
+					</div>
+				</div>
+				<div>
+					<h2>Welcome to B-tiles</h2>
+				</div>
+				<div className = 'layout'>
+
+					<h3>50 years of experience.</h3>
+					<img src = 'ssfygiuhu' alt = 'image' />
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div>
+					<div>
+						<img src = 'ssfygiuhu' alt = 'image' />
+						<h4>Top Rated</h4>
+						<p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+							 Officia odio eum corporis consequatur.</p>
+					</div>
+					<div>
+						<img src = 'ssfygiuhu' alt = 'image' />
+						<h4>Best Quality</h4>
+						<p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+							 Officia odio eum corporis consequatur.</p>
+					</div>
+					<div>
+						<img src = 'ssfygiuhu' alt = 'image' />
+						<h4>Low Cost</h4>
+						<p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+							 Officia odio eum corporis consequatur.</p>
+					</div>
+					
+				</div>
 			</div>
+
+
 		) ;
 	}
 }
 
 export default About;
+                         
+
+                       
