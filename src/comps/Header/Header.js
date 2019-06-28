9180f7a158942a36e72923c61792dc47dfac0837
manan999 <aggarwalmanan4@gmial.com>
2019-06-28 14:29:40 +0530

    header started

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -1,11 +1,37 @@
 import React from 'react' ;
+import {Link} from'react-router-dom' ;
+
+import './header.css' ;
 
 class Header extends React.Component
-{
+{	constructor(props) {
+	    super(props)
+	    this.state = {
+	      menuOpen: false,
+	    }
+	}
+
+	openMenu = () => {
+	    this.setState({ menuOpen: true })
+	}
+
+	closeMenu = () => {
+		//console.log("trigger") ;
+	    this.setState({ menuOpen: false })
+	}
+
 	render()
 	{
 		return (
-			<div> This is Header </div>
+			<div className="topbar" id="bar"> 
+				<Link to="/" className="header-item" id="logo"> MARVEL </Link> 
+				<div className = "right-header">
+					<Link className="header-item" to='/'> Home </Link>
+					<Link className="header-item" to='/about'> About Us </Link>
+					<Link className="header-item" to='/products'> Products </Link>
+					<Link className="header-item" to='/contact'> Contact </Link>
+				</div>
+			</div>
 			) ;
 	}
 }
--- src/comps/Header/TopBar.js
+++ src/comps/Header/TopBar.js
@@ -1,5 +1,4 @@
 import React from 'react' ;
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
 import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons' ;
 import { faFacebookSquare, faWhatsapp, faRedditAlien } from '@fortawesome/free-brands-svg-icons' ;
 
@@ -14,7 +13,7 @@ class TopBar extends React.Component
 			<div className="topbar"> 
 				<BarItem link="tel:09564328921" text=" +91 - 9564-328-921" icon={faPhoneAlt}/>
 				<BarItem link="mailto:b.tiles@gmail.com" text=" b.tiles@gmail.com" icon={faEnvelope}/>
-				<div className="right-top-bar">
+				<div className="right-topbar">
 					<BarItem link="https://www.facebook.com" logo="logo" icon={faFacebookSquare}/>
 					<BarItem link="https://www.reddit.com" logo="logo" icon={faRedditAlien}/>
 					<BarItem link="https://www.reddit.com" logo="logo" icon={faWhatsapp}/>
