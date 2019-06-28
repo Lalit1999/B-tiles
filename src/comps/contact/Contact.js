1dcd88a3278547897fa1ac81899268fbc9572857
Lalit1999 <9lalitkumar9@gmail.com>
2019-06-28 21:21:29 +0530

    added contact.js content again

--- src/comps/contact/Contact.js
+++ src/comps/contact/Contact.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Contact.css' ;
 import '../about/About.css' ;
 
+import Image4 from '../images/dummy-map.jpg' ;
+
+
 class Contact extends React.Component
 {
 	
@@ -16,7 +19,21 @@ class Contact extends React.Component
 
 						<div className = 'layout'>Contact-Us</div> 
 					</div>
-				</div>	      		
+				</div>	
+				<div>
+				<img src = {Image4} alt = "map" />
+					<h3>Leave Your Message</h3>
+					<input type = "text" placeholder = "Your Name" />
+					<input type = "text" placeholder = "Your Email" />
+					<input type = "text" placeholder = "Your Message" />
+					<button type = "submit">Send</button>
+				</div>
+				<div>
+					<h3>Contact Information</h3>
+					<p>yaha address aayega</p>
+					<p>yaha phone number</p>
+					<p>yaha email-address ayega</p>
+				</div>      		
 			</div>
 		) ;
 	}
