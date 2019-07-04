77365cf6bdb0e655a10b873366f29430a83eb052
manan999 <aggarwalmanan4@gmial.com>
2019-07-04 22:47:30 +0530

    brands slider adjusted

--- src/comps/Home/Home.js
+++ src/comps/Home/Home.js
@@ -8,6 +8,15 @@ import BrandSlider from '../Slider/BrandSlider.js' ;
 import wall from '../images/wall.jpg' ;
 import floor from '../images/floor.jpg' ;
 import exterior from '../images/it1.jpg' ;
+import eco from '../images/brand/ecoflex.jpg' ;
+import jell from '../images/brand/jell.png' ;
+import john from '../images/brand/johnson.png' ;
+import kaj from '../images/brand/kajaria.jpg' ;
+import lg from '../images/brand/lg.png' ;
+import somany from '../images/brand/somany.jpg' ;
+import wonder from '../images/brand/wonder.png' ;
+
+let data = [lg, somany, kaj, wonder, eco, jell, john] ;
 
 const tiles = [
 	{
@@ -78,7 +87,7 @@ class Home extends React.Component
 				<Banner title="Residential" items={tiles}/>
 				<Banner title="Commercial" items={tiles2}/>
 				<Parallax text="Build your dream home with B-Tiles" />
-				<BrandSlider />
+				<BrandSlider data={data} title="OUR BRANDS"/>
 				<div> This is Home </div>
 			</div>
 			) ;
--- src/comps/Slider/BrandSlider.js
+++ src/comps/Slider/BrandSlider.js
@@ -3,22 +3,12 @@ import Swiper from 'react-id-swiper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons' ;
 
-import eco from '../images/brand/ecoflex.jpg' ;
-import jell from '../images/brand/jell.png' ;
-import john from '../images/brand/johnson.png' ;
-import kaj from '../images/brand/kajaria.jpg' ;
-import lg from '../images/brand/lg.png' ;
-import somany from '../images/brand/somany.jpg' ;
-import wonder from '../images/brand/wonder.png' ;
-
 import './slider.css' ;
-
-let data = [lg, somany, kaj, wonder, eco, jell, john] ;
 
 class BrandSlider extends Component
 {	
 	dataList = () => {
-		return /*this.props.*/data.map((item, i) => {
+		return this.props.data.map((item, i) => {
 			return (
 				<div className="swiper-slide2" key={i}>
 					<div className="slide2" >
@@ -73,7 +63,7 @@ class BrandSlider extends Component
 		return (
 			<div className="slider-parent2">
 				<div className='brand-heading'>
-					__________<ins> OUR BRANDS </ins>__________ 
+					__________<ins> {this.props.title} </ins>__________ 
 				</div>
 				<div className="brand-con">
 					<Swiper {...params}>
