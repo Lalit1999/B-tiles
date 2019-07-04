bc25b4c59fb5722434080e3bec1e0822e2a9ca7e
Lalit1999 <9lalitkumar9@gmail.com>
2019-07-04 11:42:19 +0530

    added flex component

--- src/comps/about/About.js
+++ src/comps/about/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css' ;
 import Title from '../title/Title.js' ; 
+import Flex from '../flex/Flex.js' ;
 
 import Image2 from '../images/int3.jpg' ;
 import Image3 from '../images/interior.png' ;
@@ -54,26 +55,39 @@ class About extends React.Component
 					</div>
 				</div>
 				<div className = 'layout1'>
-					<div >
-						<img src = {Image2}  alt = 'image2' width = '50' height = '50'/>
-						<h4>Top Rated</h4>
-						<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
-							 Officia odio eum corporis consequatur.</p>
+					<div className = 'sb' >
+						<div>
+							<img className = 'margin' src = {Image2}  alt = 'image2' width = '50' height = '50'/>
+						</div>
+						<div>
+							<h4>Top Rated</h4>
+							<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+								 Officia odio eum corporis consequatur.</p>
+						</div>
 					</div>
-					<div >
-						<img src = {Image3} alt = 'image3' width = '50' height = '50' />
-						<h4>Best Quality</h4>
-						<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
-							 Officia odio eum corporis consequatur.</p>
+					<div className = 'sb' >
+						<div>
+							<img className = 'margin' src = {Image3} alt = 'image3' width = '50' height = '50' />
+						</div>
+						<div>
+							<h4>Best Quality</h4>
+							<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+								 Officia odio eum corporis consequatur.</p>
+						</div>
 					</div>
-					<div>
-						<img src = {Image4} alt = 'image4' width = '50' height = '50' />
-						<h4>Low Cost</h4>
-						<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
-							 Officia odio eum corporis consequatur.</p>
+					<div className = 'sb '>
+						<div>
+							<img className = 'margin' src = {Image4} alt = 'image4' width = '50' height = '50' />
+						</div>
+						<div>
+							<h4>Low Cost</h4>
+							<p width = '5'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+								 Officia odio eum corporis consequatur.</p>
+						</div>
 					</div>
 					
 				</div>
+				<Flex />
 			</div>
 
 
---
+++ src/comps/flex/Flex.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import './Flex.css' ;
+
+class Flex extends React.Component
+{
+	render()
+	{
+		return(
+			<div className = 'rang'>
+				this is flex.
+			</div>
+		) ;
+	}
+}
+
+export default Flex ;
